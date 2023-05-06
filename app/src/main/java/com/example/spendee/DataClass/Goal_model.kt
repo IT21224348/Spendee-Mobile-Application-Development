c3203f78b3795575ee5a824b518acaa7d24cf193
Ravindu Yasith <99459699+It21225710@users.noreply.github.com>
2023-05-06 17:54:23 +0530

    goal model class were added

---
+++ app/src/main/java/com/example/spendee/DataClass/Goal_model.kt
@@ -0,0 +1,7 @@
+package com.example.spendee.DataClass
+
+data class Goal_model(val name:String? = null,
+                      val  date: String? = null,
+                      val target:Double= 0.00,
+                      val balance:Double= 0.00)
+
