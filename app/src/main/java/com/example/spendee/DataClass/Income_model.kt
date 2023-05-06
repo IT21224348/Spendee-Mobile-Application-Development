56c061cedb06a740c7148bf6c251ba62bfb0f372
Jeewantha De Silva <100590888+IT21228612@users.noreply.github.com>
2023-05-06 21:04:24 +0530

    expense and income models added
    final versions of expense and income models added

---
+++ app/src/main/java/com/example/spendee/DataClass/Income_model.kt
@@ -0,0 +1,6 @@
+package com.example.spendee.DataClass
+
+data class Income_model( val name:String? = null,
+                         val amount:Double= 0.00,
+                         val  category: String? = null,
+                         val settle_date: String? = null)
