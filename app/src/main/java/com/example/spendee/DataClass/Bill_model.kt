c85d7d884d3d741cefc2c5c0f04cfc18156e24b7
Dulhan Karunarathne <99629913+IT21228858@users.noreply.github.com>
2023-05-07 13:35:44 +0530

    bill model added
    bill model was refined and added

---
+++ app/src/main/java/com/example/spendee/DataClass/Bill_model.kt
@@ -0,0 +1,5 @@
+package com.example.spendee.DataClass
+
+data class Bill_model(val name:String? = null,
+                      val target:Double= 0.00,
+                      val  date: String? = null,)
